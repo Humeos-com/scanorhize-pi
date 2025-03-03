a2ec2d3fbcbf297d839395b1c114127b44385f2d
arditial <alain@arditi.fr>
2025-03-03 11:29:48 +0100

    Detection MEGA4

--- OSUtils.py
+++ OSUtils.py
@@ -6,13 +6,26 @@ import os
 import platform
 import json
 import logging
+import usb.core
 
+# Carte UUGEAR MEGA4
+# 2109:2817 VIA Labs, Inc. USB2.0 Hub, USB 2.10, 4 ports, ppps
+def get_MEGA4():
+
+    with_MEGA4 = True
+    dev = usb.core.find(idVendor=0x2109, idProduct=0x2817)
+    if dev is None:
+        print("Pas de carte MEGA4")
+        with_MEGA4 = False
+    return with_MEGA4
+	
 class Config:
     ''' Permet de connaitre quel est l'environnement d'exécution du programme '''
     _instance = None  # Class variable to store the single instance
     environment = "PROD"
     log_level = "INFO"
     platform = "Linux"
+    usb_mode = "GPIO"
 
     def __new__(cls):
         if cls._instance is None:
@@ -47,6 +60,11 @@ class Config:
                 if "Raspberry Pi" in f.read():
                     cls.platform = "Raspberry"
 
+        if get_MEGA4():
+            cls.usb_mode = "MEGA4"
+        else:
+            cls.usb_mode = "GPIO"
+
     @classmethod
     def _setup_logging(cls):
         """Configure logging based on environment"""
@@ -76,6 +94,13 @@ class Config:
             return True
         return False
 
+    @classmethod
+    def has_MEGA4(cls):
+        if cls.usb_mode == "MEGA4":
+            return True
+        return False
+        
+
 def is_dev():
     return Config().is_dev()
 
@@ -92,9 +117,12 @@ def get_os():
         return "MacOS"
     return "Linux"
 
+def has_MEGA4():
+    return Config().has_MEGA4()
 
 if __name__ == "__main__":
     print(get_os())
     print(f"Dev: {is_dev()}")
     print(f"Debug: {is_debug()}")
     print(f"Raspberry: {is_raspberry_pi()}")
+    print(f"has MEGA4: {has_MEGA4()}")
