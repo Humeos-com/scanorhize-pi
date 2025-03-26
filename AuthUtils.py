5dbefd5ec9fc83d32b930e1daa73c39345c01ea0
Alain ARDITI <alain@arditi.fr>
2025-03-26 14:40:09 +0000

    Recuperation de la MacAddress du Raspberry

--- AuthUtils.py
+++ AuthUtils.py
@@ -66,25 +66,17 @@ class AuthenticationData:
         """Set initialization status"""
         self.__initialized = value
 
-
-def getHwAddr():
-    """Renvoie l'adresse MAC de l'interface réseau eth0 pour les Raspberry Pi
-    quelque soit l'interface réseau utilisée
-    Cette fonction ne sert que pour authentifier le boitier
-
-    Returns:
-        str: la Mac Address du boitier
-    """
-    if not is_raspberry_pi():
-        return "0A:0B:00C:0D:0E:0F"
-
+def getHwAddr(ifname=IFACE):
     s = socket(AF_INET, SOCK_DGRAM)
-    info = ioctl(s.fileno(), 0x8927, pack("256s", IFACE[:15]))
-    return ":".join(f"{ord(char):02x}" for char in info[18:24])
-    # return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+    # Convert ifname name to bytes using `.encode()`
+    return ':'.join('%02x' % b for b in ioctl(
+        s.fileno(),
+        0x8927,  # SIOCGIFHWADDR (Get MAC Address)
+        pack('256s', ifname[:15].encode("utf-8"))  # Convert to bytes
+    )[18:24])
 
 
-def getIPAddr(ifname):
+def getIPAddr(ifname=IFACE):
     if ifname == "lo":
         return "127.0.0.1"
     if not is_raspberry_pi():
@@ -92,10 +84,10 @@ def getIPAddr(ifname):
 
     s = socket(AF_INET, SOCK_DGRAM)
     try:
-        info = ioctl(s.fileno(), 0x8915, pack("256s", ifname[:15]))
+        info = ioctl(s.fileno(), 0x8915, pack("256s", ifname[:15].encode("utf-8")))
     except IOError as e:
         print(f"IOError: {e}")
-        return "No address assigned"
+        return None
     return inet_ntoa(info[20:24])
 
 
@@ -112,5 +104,5 @@ if __name__ == "__main__":
     for iface in f:
         print("Interface:", iface)
         print("IPv4 Addr:", getIPAddr(iface))
-        print("HW MAC Addr:", getHwAddr())
+        print("HW MAC Addr:", getHwAddr(iface))
         print("-----------------------------")
