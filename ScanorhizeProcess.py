8d03c03fe32ae90f501c1e7d038fd473390eeb18
Alain ARDITI <alain@arditi.fr>
2025-03-28 23:52:33 +0100

    Les heures du WittyPi sont chargées au démarrage de ScanorhizeStart

--- ScanorhizeProcess.py
+++ ScanorhizeProcess.py
@@ -5,69 +5,51 @@ Main process for Scanorhize: fait l'acquisition,
 
 from time import sleep
 
+from ConfigApp import getLogger
 from Scanner import listConfigScanner, scanAcq, ScannerData
 
 from Server import (
     HubData,
-    getTokens,
     SendParameters,
     PostImageToServer,
     ReadConfigFromServer,
 )
 from Miscellaneous import (
-    WriteTimeLogfile,
     InitGPIO,
     WriteStartDateConfig,
     ReadStartDateConfig,
     initDisplayFile,
 )
 from Campaign import CreateFolderImage, CopyImageToUSB, USBSpace
-from WittyPython import ReadBatVoltCap, ReadTemp
 from DateUtils import CalculNextStartDate, DateToSeconds, GetCurrentDate
 
 DateStart = GetCurrentDate()
 initDisplayFile()
-WriteTimeLogfile("StartProcess")
+getLogger().warning("StartProcess")
 res = InitGPIO()
 if res != 0:
-    WriteTimeLogfile("InitGPIOError")
+    getLogger().error("InitGPIOError")
 
 # init
 Scanner = ScannerData()
 NextStartseconds = [0, 0, 0]
-NextStartDate = [" ", " ", " "]
+NextStartDates = [" ", " ", " "]
 
 listScannerconfigs = listConfigScanner()
 scanning = 0
 internet = 1
-NextStartDate = ReadStartDateConfig()
+NextStartDates = ReadStartDateConfig()
 i_scan = 0
-for dates in NextStartDate:
+for dates in NextStartDates:
     NextStartseconds[i_scan] = DateToSeconds(dates)
     i_scan = i_scan + 1
 
-# Paramètres à envoyer au début du process
-# A faire dans le serveur Flask pour initialiser les données
-# getTokens()
-Hub_ = HubData()
-Hub_.ReadConfig()
-volt, Hub_.batteryLevelPercent = ReadBatVoltCap()
-Hub_.diskSpacePercent = USBSpace()[0]
-Hub_.temperature = ReadTemp()
-Hub_.WriteConfig()
-
-WriteTimeLogfile(
-    f"Bat: {Hub_.batteryLevelPercent}  "
-    f"USB: {Hub_.diskSpacePercent}  "
-    f"Temp: {Hub_.temperature}"
-)
-SendParameters(Hub_)
 
 i_scan = 0
 for CurrentScanner in listScannerconfigs:
     Scanner.ReadScannerConfig(CurrentScanner)
     data = "scanner file: " + CurrentScanner
-    WriteTimeLogfile(data)
+    getLogger().warning(data)
 
     DateOriginS = DateToSeconds(Scanner.StartDate)
     CurrentDateinS = DateToSeconds(DateStart)
@@ -81,49 +63,48 @@ for CurrentScanner in listScannerconfigs:
         + " "
         + str(CurrentDateinS)
         + " NextStart : "
-        + str(NextStartDate[i_scan])
+        + str(NextStartDates[i_scan])
     )
-    WriteTimeLogfile(data)
+    getLogger().warning(data)
 
     if CurrentDateinS > DateOriginS and CurrentDateinS >= NextStartseconds[i_scan]:
-        WriteTimeLogfile("Turn scanner" + str(i_scan + 1) + " On")
+        getLogger().warning("Turn scanner %s On", str(i_scan + 1))
 
         # get image and post image
-        WriteTimeLogfile("Start image acquisition")
+        getLogger().warning("Start image acquisition")
         Scanner = scanAcq(Scanner, i_scan, DateStart)
         Scanner.WriteScannerConfig(CurrentScanner)
         scanning = 1
         if Scanner.error == 0:
-            WriteTimeLogfile("Image acquisition Ok")
+            getLogger().warning("Image acquisition Ok")
             sleep(5)
             FolderImage = CreateFolderImage(Scanner.Campaign, i_scan)
             copyerror = CopyImageToUSB(Scanner, FolderImage)
             if copyerror == 0:
-                WriteTimeLogfile("Image copied to USB")
+                getLogger().warning("Image copied to USB")
             else:
-                WriteTimeLogfile("Error in copy to USB")
+                getLogger().error("Error in copy to USB")
 
-            WriteTimeLogfile("StartPostImage")
+            getLogger().warning("StartPostImage")
             PostError = PostImageToServer(Scanner)
             if PostError != 0:
-                WriteTimeLogfile("Post error")
+                getLogger().error("Post error")
             else:
-                WriteTimeLogfile("EndPost OK")
+                getLogger().warning("EndPost OK")
         else:
-            WriteTimeLogfile("Image acquisition Error")
+            getLogger().error("Image acquisition Error")
     # Prepare next StartDate for Scanner
     if Scanner.UseServer == 1:
-        data = "Scanner config from server"
-        WriteTimeLogfile(str(data))
+        getLogger().warning("Get Scanner config from server")
         Scanner = ReadConfigFromServer(Scanner)
         Scanner.WriteScannerConfig(listScannerconfigs[i_scan])
 
     nextDate, nextDateS = CalculNextStartDate(
         Scanner.StartDate, Scanner.PeriodeS, DateStart
     )
-    NextStartDate[i_scan] = nextDate
-    WriteTimeLogfile("Next start date: " + nextDate)
+    NextStartDates[i_scan] = nextDate
+    getLogger().warning("Next start date: %s", nextDate)
     i_scan = i_scan + 1
 # fin for
 
-WriteStartDateConfig(NextStartDate)
+WriteStartDateConfig(NextStartDates)
--- ScanorhizeStart.py
+++ ScanorhizeStart.py
@@ -4,27 +4,31 @@ import sys
 from subprocess import run, CalledProcessError
 
 from WittyPy import SetNextStartDate, doShutdown, setNextShutdownDate
-from WittyPython import ReadBatVoltCap
+from WittyPython import ReadBatVoltCap, ReadTemp
 from Miscellaneous import (
     InitGPIO,
     EndGPIO,
     TurnUsbOn,
     ReadGPIOConfig,
-    WriteTimeLogfile,
-    ReadStartDateConfig,
 )
-from DateUtils import GetCurrentDate, SecondsToDate, DateToSeconds
-from OSUtils import is_dev
+from DateUtils import GetCurrentDate, SecondsToDate, DateToSeconds, CalculNextStartDate
+from ConfigApp import is_dev, getLogger
+from Scanner import listConfigScanner, ScannerData
+from Server import HubData, SendParameters
+from Campaign import USBSpace
 
-WriteTimeLogfile("ScanorhizeStart.py")
+
+
+getLogger().warning("ScanorhizeStart.py")
 
 res = InitGPIO()
 if res != 0:
-    WriteTimeLogfile("InitGPIOError")
+    getLogger().error("InitGPIOError")
 # On allume la clé 4G
 res = TurnUsbOn(3, 0)
 if res != 0:
-    WriteTimeLogfile("TurnUsbOnError")
+    getLogger().error("TurnUsbOnError")
+
 
 config = ReadGPIOConfig()
 EndGPIO()
@@ -49,76 +53,119 @@ EndGPIO()
 if config == 0:
     # En mode config
     cmd = "sudo python3 Scanorhize.py &"
-    WriteTimeLogfile(cmd)
+    getLogger().warning(cmd)
     run(cmd, capture_output=True, universal_newlines=True, shell=True, check=False)
-    sys.exit(1)
+    sys.exit(0)
 
-
+# Etape 1 #############################################
+# Mise à jour des dates de réveil et d'arrêt du WittyPi
 DateStart = GetCurrentDate()
 CurrentDateinS = DateToSeconds(DateStart)
-NextDate = SecondsToDate(CurrentDateinS + (3600 * 1))  # demarrage toutes les 1h
-StopTime = SecondsToDate(
-    CurrentDateinS + (60 * 20)
-)  # arret au bout de 20min par sécurité en cas d'erreur
-WriteTimeLogfile("Set NextDate: " + NextDate)
-SetNextStartDate(NextDate)
 
-# pylint: disable=wrong-import-position, disable=unused-import
-# On execute le contenu du fichier ScanorhizeProcess.py
-# On scanne les images et on les envoie à la plateforme Web
-cmd = "python3 ScanorhizeProcess.py"
-WriteTimeLogfile(cmd)
-try:
-    result = run(
-        cmd, capture_output=True, universal_newlines=True, shell=True, check=True
-    )
-except CalledProcessError as exc:
-    WriteTimeLogfile(exc.stderr)
-
-## On ne lance pas par l'import, car s'il y a une erreur, le programme s'arrête
-## import ScanorhizeProcess
-
-# On détermine la prochaine date de réveil
+# On détermine la prochaine date de réveil à partir du fichier NextStartDate.json
 NextStartseconds = [0, 0, 0]
-NextStartDate = [" ", " ", " "]
+NextStartDates = [" ", " ", " "]
 
-NextStartDate = ReadStartDateConfig()
-date_now = GetCurrentDate()
-secs_now = DateToSeconds(date_now)
+
+# A mettre à jour avec les données des scanners
+Scanner = ScannerData()
+listScannerconfigs = listConfigScanner()
 i_scan = 0
-for dates in NextStartDate:
-    NextStartseconds[i_scan] = DateToSeconds(dates)
-    i_scan = i_scan + 1
+for CurrentScanner in listScannerconfigs:
+    Scanner.ReadScannerConfig(CurrentScanner)
+    DateOriginS = DateToSeconds(Scanner.StartDate)
+    nextDate, nextDateS = CalculNextStartDate(
+        Scanner.StartDate, Scanner.PeriodeS, DateStart
+    )
+    NextStartDates[i_scan] = nextDate
+    NextStartseconds[i_scan] = nextDateS
+    getLogger().warning("Scanner-%s: Next start date: %s", i_scan+1, nextDate)
+    i_scan += 1
 
 # Quand la carte WittyPi n'a plus de batterie, son heure interne est aléatoire.
 # Lorsque les acquisitions ne fonctionnent pas, la nextStartDate ne bouge pas.
 # Donc elle se retrouve dans le passé. Avec un minimum de secs_now + 600, on
 # espère que la carte WittyPi se réveillera.
-nextStartSecs = max(int(min(NextStartseconds)), (secs_now + 600))
+nextStartSecs = max(int(min(NextStartseconds)), (CurrentDateinS + 600))
 nextStartDateValue = SecondsToDate(nextStartSecs)
-WriteTimeLogfile("Next start at: " + nextStartDateValue)
+getLogger().warning("Next start at: %s", nextStartDateValue)
 
 Bat = ReadBatVoltCap()
 if Bat[1] < 0:  # si plus de batterie on ne réveille plus le système
     nextStartDateValue = SecondsToDate(nextStartSecs + (3600 * 24 * 30))  # +30 jours
-    WriteTimeLogfile("No more battery: start in 30 days")
+    getLogger().warning("No more battery: start in 30 days: %s", nextStartDateValue)
 
 SetNextStartDate(nextStartDateValue)
 
+StopTime = SecondsToDate(
+    CurrentDateinS + (60 * 20)
+)  # arret au bout de 20min par sécurité en cas d'erreur
+setNextShutdownDate(StopTime)
+
+
+# Etape 2 #############################################
+# On lance le scan des images
+# On execute le contenu du fichier ScanorhizeProcess.py
+# On scanne les images et on les envoie à la plateforme Web
+cmd = "python3 ScanorhizeProcess.py"
+getLogger().warning(cmd)
+try:
+    result = run(
+        cmd, capture_output=True, universal_newlines=True, shell=True, check=True
+    )
+except CalledProcessError as exc:
+    getLogger().error(exc.stderr)
+
+## On ne lance pas avec l'import, car s'il y a une erreur, le programme s'arrête
+## import ScanorhizeProcess
+
+# Etape 3 #############################################
+# Peut-être à mettre dans un sous programme au cas où il y aurait des plantages
+# On échange avec la plateforme Web pour envoyer les images et les paramètres
+
+# On allume la clé 4G et on attend d'avoir le réseau
+
+
+# Paramètres à envoyer au début du process
+# A faire dans le serveur Flask pour initialiser les données
+# getTokens()
+Hub_ = HubData()
+Hub_.ReadConfig()
+volt, Hub_.batteryLevelPercent = ReadBatVoltCap()
+Hub_.diskSpacePercent = USBSpace()[0]
+Hub_.temperature = ReadTemp()
+Hub_.WriteConfig()
+
+getLogger().warning(
+    "Bat: %s  USB: %s  Temp: %s",
+    Hub_.batteryLevelPercent,
+    Hub_.diskSpacePercent,
+    Hub_.temperature,
+)
+SendParameters(Hub_)
+
+# Ensuite on synchronise les images et les fichiers JSON
+# A faire
+
+
+# Etape 4 #############################################
+# On éteint le Raspberry Pi et le WittyPi
 if is_dev():
-    WriteTimeLogfile("Dev mode: on ne lance pas le shutdown et on n'ejecte pas la clé")
+    getLogger().warning("Dev mode: on ne lance pas le shutdown et on n'ejecte pas la clé")
     sys.exit(0)
 
 cmdeject = "sudo eject /dev/sda"
 result = run(
     cmdeject, capture_output=True, universal_newlines=True, shell=True, check=False
 )
-WriteTimeLogfile(cmdeject)
+getLogger().warning(cmdeject)
 
 # On fixe l'heure d'arrêt, car des fois le Witty ne s'eteint pas sur le doShutdown()
 # qui ne fait que le poweroff du Raspberry
+date_now = GetCurrentDate()
+secs_now = DateToSeconds(date_now)
 date_new = SecondsToDate(secs_now + 30)
-WriteTimeLogfile("Next stop at: " + date_new)
+getLogger().warning("Next stop at: %s", date_new)
 setNextShutdownDate(date_new)
 
 # lance le poweroff du Raspberry et éteint le WittyPi
