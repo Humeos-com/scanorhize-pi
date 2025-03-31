6cca8bf51b1068c26c908729157aef2adab21247
Alain ARDITI <alain@arditi.fr>
2025-03-31 13:34:10 +0000

    Adaptation pour systemd-timesyncd

--- TimeSynchronisation.sh
+++ TimeSynchronisation.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 
 
-sudo systemctl restart systemd-timesyncd.service &
+
+systemctl restart systemd-timesyncd.service &
 sleep 5
-cd /home/pi/wittypi && . utilities.sh && system_to_rtc
+. /home/pi/wittypi/utilities.sh && system_to_rtc
 timedatectl
+exit
 
 #sudo /etc/init.d/ntp stop
 #sudo ntpd -q -g
