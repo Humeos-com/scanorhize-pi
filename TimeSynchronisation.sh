5177fe93fa4682d6a20ff46346ff3e6ca6168f9e
Alain ARDITI <alain@arditi.fr>
2025-04-13 22:08:48 +0200

    On vérifier qu'on le lance en root sudo

--- TimeSynchronisation.sh
+++ TimeSynchronisation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
+# check if sudo is used
+if [ "$(id -u)" != 0 ]; then
+  echo 'Sorry, you need to run this script with sudo'
+  exit 1
+fi
 
 systemctl restart systemd-timesyncd.service &
 sleep 5
 . /home/pi/wittypi/utilities.sh && system_to_rtc
 timedatectl
-exit
-
-#sudo /etc/init.d/ntp stop
-#sudo ntpd -q -g
-#sudo /etc/init.d/ntp start
+exit 0
 
