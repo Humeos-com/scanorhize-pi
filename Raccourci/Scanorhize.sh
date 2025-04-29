95428ab608bd77324b21e3dde3fc6d2be056844f
Alain ARDITI <alain@arditi.fr>
2025-04-29 15:18:18 +0200

    Ancienne application

--- Raccourci/Scanorhize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/pi/Documents/Scanorhize/
-sudo python3 /home/pi/Documents/Scanorhize/Scanorhize.py &
-#chromium-browser --start-fullscreen 10.3.141.1:8080
-chromium-browser --start 10.3.141.1:8080
-
-
